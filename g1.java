1ad6ede088dc749a8a6f2c6c3107ea7d97cafe92
ganapatim <64065322+ganapatim@users.noreply.github.com>
2020-04-22 12:12:03 +0530

    Add files via upload

---
+++ g1.java
@@ -0,0 +1,15 @@
+//program to print first 10 numbers which are divisible by 5
+class A
+{
+	public static void main(String[]args)
+	{
+		int i=1,mul;
+		while(i<=10)
+		{
+			mul=i*5;
+			System.out.println(mul);
+			i++;
+		}
+					
+	}
+}
